f84ec070edf22d56f5685680d6234943a9026772
isamu takimoto <isamu.takimoto.136@gmail.com>
2022-10-21 23:27:19 +0900

    サイトマップの追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,26 +15,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/buzz_top', function () {
     return view('buzzTop');
-});
+})->name('buzzTop');
 Route::get('/buzz_tube', function () {
     return view('buzzTube');
-});
+})->name('buzzTop.buzzTube');
 Route::get('/buzz_nico', function () {
     return view('buzzNico');
-});
+})->name('buzzTop.buzzNico');
 
 Route::get('/investment_trust', function () {
     return view('investmentTrust');
-});
+})->name('investment_trust');
 
 Route::get('/investment_trust_statistics', function () {
     return view('investmentTrustStatistics');
-});
+})->name('investment_trust.investment_trust_statistics');
 
 Route::get('/investment_trust_statistics_graph', function () {
     return view('investmentTrustStatisticsGraph');
-});
+})->name('investment_trust.investment_trust_statistics_graph');
+
+Route::get('/sitemap.xml', 'App\Http\Controllers\SitemapController@sitemap')->name('sitemap.sitemap');
 
 Route::get('/', function () {
     return view('top');
-});
+})->name('top');
