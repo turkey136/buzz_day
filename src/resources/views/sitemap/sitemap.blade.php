5c1a21a5ba008fac54ba9194ca5e2a20d3159791
isamu takimoto <isamu.takimoto.136@gmail.com>
2022-11-11 23:34:39 +0900

    mod sitemap

--- src/resources/views/sitemap/sitemap.blade.php
+++ src/resources/views/sitemap/sitemap.blade.php
@@ -1,41 +1,8 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <sitemap>
-      <loc>{{ route('investment_trust.investment_trust_statistics_graph') }}</loc>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+      <loc>{{ route('sitemap.pageSitemap') }}</loc>
       <lastmod>{{ date("Y-m-d") }}</lastmod>
-  </sitemap>
-  <sitemap>
-      <loc>{{ route('investment_trust.investment_trust_statistics') }}</loc>
-      <lastmod>{{ date("Y-m-d") }}</lastmod>
-  </sitemap>
-  <sitemap>
-      <loc>{{ route('investment_trust') }}</loc>
-      <lastmod>{{ date("Y-m-d") }}</lastmod>
-  </sitemap>
-  <sitemap>
-      <loc>{{ route('buzzTop.buzzNico') }}</loc>
-      <lastmod>{{ date("Y-m-d") }}</lastmod>
-  </sitemap>
-  <sitemap>
-      <loc>{{ route('buzzTop.buzzTube') }}</loc>
-      <lastmod>{{ date("Y-m-d") }}</lastmod>
-  </sitemap>
-    <sitemap>
-      <loc>{{ route('buzzTop') }}</loc>
-      <lastmod>{{ date("Y-m-d") }}</lastmod>
-  </sitemap>
-  <sitemap>
-      <loc>{{ route('novel.top') }}</loc>
-      <lastmod>{{ date("Y-m-d") }}</lastmod>
-  </sitemap>
-  <sitemap>
-      <loc>{{ route('novel.yomou') }}</loc>
-      <lastmod>{{ date("Y-m-d") }}</lastmod>
-  </sitemap>
-  <sitemap>
-      <loc>{{ route('top') }}</loc>
-      <lastmod>{{ date("Y-m-d") }}</lastmod>
-  </sitemap>
-
-</sitemapindex>
+  </url>
+</urlset>
